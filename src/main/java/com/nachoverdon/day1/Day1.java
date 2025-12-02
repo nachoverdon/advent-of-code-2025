4936417561fefcf11023c359bf9139988c724c79
nachoverdon <nverblaz@gmail.com>
2025-12-02 16:04:39 +0100

    first commit (days 1 and 2)

---
+++ src/main/java/com/nachoverdon/day1/Day1.java
@@ -0,0 +1,68 @@
+package com.nachoverdon.day1;
+
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+record Rotation(int dir, int dist) {}
+
+public class Day1 {
+  private static final int PART = 2;
+  private static final String RESOURCES = "src/main/resources/day1/";
+  private static final int L = -1;
+  private static final int R = 1;
+  private int pos = 50;
+  private int password = 0;
+
+  static void main() {
+    Day1 test = new Day1();
+    test.execute(RESOURCES + "day1_test.txt");
+    System.out.println("Test password is: " + test.password);
+
+    Day1 exercise = new Day1();
+
+    exercise.execute(RESOURCES + "day1_input.txt");
+    System.out.println("Password is: " + exercise.password);
+  }
+
+  private Rotation parseRotation(String line) {
+    int dir = "L".equals(line.substring(0, 1)) ? L : R;
+    int dist = Integer.parseInt(line.substring(1));
+
+    return new Rotation(dir, dist);
+  }
+
+  private void execute(String fileName) {
+    Path path = Paths.get(fileName);
+
+    try (BufferedReader reader = Files.newBufferedReader(path)) {
+      if (PART == 1) {
+        reader.lines().map(this::parseRotation).forEachOrdered(this::processRotation);
+      } else if (PART == 2) {
+        reader.lines().map(this::parseRotation).forEachOrdered(this::processRotationLoop);
+      }
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
+  // Simple implementation that goes step by step.
+  private void processRotationLoop(Rotation rot) {
+    for (int i = 0; i < rot.dist(); i++) {
+      pos += rot.dir();
+
+      if (pos == -1) pos = 99;
+      else if (pos == 100) pos = 0;
+
+      if (pos == 0) password++;
+    }
+  }
+
+  private void processRotation(Rotation rot) {
+    pos = (pos + (rot.dist() * rot.dir()) % 100 + 100) % 100;
+
+    if (pos == 0) password++;
+  }
+}
---
+++ src/main/java/com/nachoverdon/day2/Day2.java
@@ -0,0 +1,102 @@
+package com.nachoverdon.day2;
+
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+record Range(long start, long end) {}
+
+public class Day2 {
+  private static final int PART = 2;
+  private static final Pattern pattern = Pattern.compile("^(\\d+)(?:\\1" + (PART == 2 ? "+" : "") + ")$");;
+  private static final String RESOURCES = "src/main/resources/day2/";
+  private final List<String> invalidIds = new ArrayList<>();
+  private long result = 0;
+
+  static void main() {
+    Day2 test = new Day2();
+    Day2 exercise = new Day2();
+
+    test.execute(RESOURCES + "day2_test.txt");
+    System.out.println(String.join(", ", test.invalidIds));
+    System.out.println("Test result is: " + test.result);
+
+    exercise.execute(RESOURCES + "day2_input.txt");
+    System.out.println(String.join(", ", exercise.invalidIds));
+    System.out.println("Result is: " + exercise.result);
+  }
+
+  private void execute(String fileName) {
+    Path path = Paths.get(fileName);
+
+    try (BufferedReader reader = Files.newBufferedReader(path)) {
+      String input = reader.lines().findFirst().orElseThrow();
+      String[] ranges = input.split(",");
+
+      Arrays.stream(ranges).parallel().forEach(this::processRange);
+
+      result = sumInvalidIds(invalidIds);
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
+  private void processRange(String rangeString) {
+    invalidIds.addAll(getInvalidIds(getRangeFromString(rangeString)));
+  }
+
+  private Range getRangeFromString(String range) {
+    String[] numbers = range.split("-");
+
+    return new Range(Long.parseLong(numbers[0]), Long.parseLong(numbers[1]));
+  }
+
+  private List<String> getInvalidIds(Range range) {
+    List<String> ids = new ArrayList<>();
+
+    for (long id = range.start(); id < range.end()+1; id++) {
+      // if (isInvalidSplit(id)) ids.add(Long.toString(id));
+      if (isInvalidRegex(id)) ids.add(Long.toString(id));
+    }
+
+    return ids;
+  }
+
+  private boolean isInvalidSplit(long id) {
+    String stringId = Long.toString(id);
+    int length = stringId.length();
+
+    if (length % 2 != 0) return false;
+
+    int half = length / 2;
+
+    return stringId.substring(0, half).equals(stringId.substring(half));
+  }
+
+  private boolean isInvalidRegex(long id) {
+    String stringId = Long.toString(id);
+    Matcher matcher = pattern.matcher(stringId);
+
+    int matches = 0;
+    while (matcher.find()) {
+      matches++;
+    }
+
+    return matches > 0;
+  }
+
+  private long sumInvalidIds(List<String> invalidIds) {
+    return invalidIds.parallelStream()
+        .map(Long::parseLong)
+        .reduce(Long::sum)
+        .orElse(0L);
+  }
+}
